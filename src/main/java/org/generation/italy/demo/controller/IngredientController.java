421abdb52f117f6fb664d770637557e22990ca66
ErJump <fedelijunior@gmail.com>
2022-12-13 17:05:58 +0100

    feat: :sparkles: Adds store method to ingredient controller

--- src/main/java/org/generation/italy/demo/controller/IngredientController.java
+++ src/main/java/org/generation/italy/demo/controller/IngredientController.java
@@ -1,5 +1,6 @@
 package org.generation.italy.demo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.generation.italy.demo.pojo.Ingredient;
@@ -10,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/ingredient")
@@ -38,5 +42,15 @@ public class IngredientController {
 		model.addAttribute("ingredient", i);
 		model.addAttribute("pizzas", pizzas);
 		return "ingredient-create";
+	}
+	
+	@PostMapping("/create")
+	public String storeIngredient(@Valid Ingredient ingredient) {
+		
+		List<Pizza> ingredientPizzas = ingredient.getPizzas();
+		for (Pizza p : ingredientPizzas)
+			p.getIngredients().add(ingredient);
+		iS.save(ingredient);
+		return "redirect:/ingredient";
 	}
 }
